667fbc49f509ca5b03173020198cbb49cb0cc0e9
PlayDay <18056374+playday3008@users.noreply.github.com>
2023-12-04 21:24:32 +0100

    Add x86 OpenWRT

---
+++ scripts/exec/openwrt-amd64.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Changing to working directory
+cd $(dirname "$0")
+source ../config.sh
+
+# Temp paths and files
+OPENWRT_VNUM="23.05.2"
+export SRC_PKG="https://downloads.openwrt.org/releases/${OPENWRT_VNUM}/targets/x86/64/openwrt-${OPENWRT_VNUM}-x86-64-generic-squashfs-combined.img.gz"
+export TRG_NME="openwrt"
+export TRG_PKG=$TRG_NME".img.gz"
+
+# Download syslinux and deploy it
+. ./common/download.sh
+TRG_PATH=$TFTP_PATH/boot/$TRG_NME
+[ ! -d $TRG_PATH ] && mkdir $TRG_PATH -p
+gunzip -c $TMP/$TRG_PKG > $TFTP_PATH/boot/$TRG_NME/$TRG_NME"-x86_64.img"
+
+# Clean
+. ./common/clean.sh
