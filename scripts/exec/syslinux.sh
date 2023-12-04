90d4fdb9d542b6f689a0676f354779d190c100a7
PlayDay <18056374+playday3008@users.noreply.github.com>
2023-12-04 17:53:28 +0100

    Fix syslinux

--- scripts/exec/syslinux.sh
+++ scripts/exec/syslinux.sh
@@ -11,6 +11,14 @@ rm -rf $TFTP_PATH/syslinux/*
 ln -s /usr/lib/syslinux/* $TFTP_PATH/syslinux/
 ln -s /usr/lib/PXELINUX/* $TFTP_PATH/syslinux/
 
+# Use bios folder as default
+TRG_NME="bios"
+ln -s $TFTP_PATH/syslinux/modules/$TRG_NME/* $TFTP_PATH/syslinux/
+
+# Remove existing symlinks
+[ -L $TFTP_PATH/pxelinux.0 ] && rm $TFTP_PATH/pxelinux.0
+[ -L $TFTP_PATH/pxelinux ] && rm $TFTP_PATH/pxelinux
+
 # Creating new ones
 ln -s $TFTP_PATH/$TRG_NME/pxelinux.0 $TFTP_PATH/pxelinux.0
 ln -s $TFTP_PATH/pxelinux.0 $TFTP_PATH/pxelinux
