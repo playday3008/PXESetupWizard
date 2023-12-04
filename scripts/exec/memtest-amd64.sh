743dff9667d0e49758e969cdf1aca437caea9510
PlayDay <18056374+playday3008@users.noreply.github.com>
2023-12-04 17:01:22 +0100

    Update memtest86+

--- scripts/exec/memtest-amd64.sh
+++ scripts/exec/memtest-amd64.sh
@@ -5,16 +5,16 @@ cd $(dirname "$0")
 . ../config.sh
 
 # Version of memtest
-MT_VER="memtest86+-5.01"
+MT_VER="mt86plus_6.20"
 
 # Temp paths and files
-export SRC_PKG="http://www.memtest.org/download/5.01/"$MT_VER".bin.gz"
-export TRG_NME=$MT_VER
-export TRG_PKG=$TRG_NME".bin.gz"
+export SRC_PKG="https://www.memtest.org/download/v6.20/"$MT_VER".binaries.zip"
+export TRG_NME="memtest64"
+export TRG_PKG=$TRG_NME".binaries.zip"
 
 # Create tmp path
 . ./common/download.sh
-gunzip $TMP/$TRG_PKG
+unzip $TMP/$TRG_PKG
 [ ! -d $TFTP_PATH/boot/memtest ] && mkdir $TFTP_PATH/boot/memtest
 mv $TMP/$TRG_NME".bin" $TFTP_PATH/boot/memtest/memtest
 
---
+++ scripts/exec/memtest-i386.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Changing to working directory
+cd $(dirname "$0")
+. ../config.sh
+
+# Version of memtest
+MT_VER="mt86plus_6.20"
+
+# Temp paths and files
+export SRC_PKG="https://www.memtest.org/download/v6.20/"$MT_VER".binaries.zip"
+export TRG_NME="memtest32"
+export TRG_PKG=$TRG_NME".binaries.zip"
+
+# Create tmp path
+. ./common/download.sh
+unzip $TMP/$TRG_PKG
+[ ! -d $TFTP_PATH/boot/memtest ] && mkdir $TFTP_PATH/boot/memtest
+mv $TMP/$TRG_NME".bin" $TFTP_PATH/boot/memtest/memtest
+
+. ./common/clean.sh
