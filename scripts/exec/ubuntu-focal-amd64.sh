739ffd026604d08db8c8f5b63257ea9ea23e2db3
PlayDay <18056374+playday3008@users.noreply.github.com>
2023-12-04 21:59:55 +0100

    Ubuntu 20.04 LTS

---
+++ scripts/exec/ubuntu-focal-amd64.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Changing to working directory
+cd $(dirname "$0")
+. ../config.sh
+
+export REL="focal"
+export ARCH="amd64"
+
+# Temp paths and files
+export SRC_PKG="http://archive.ubuntu.com/ubuntu/dists/$REL/main/installer-$ARCH/current/legacy-images/netboot/ubuntu-installer/$ARCH/"
+export TRG_NME="ubuntu"
+export TRG_PKG=$TRG_NME
+
+PRE_PATH=`pwd`
+
+[ ! -d $TMP/$TRG_PKG ] && mkdir $TMP/$TRG_PKG -p
+cd $TMP/$TRG_PKG
+wget -r -nH --no-parent $SRC_PKG --reject="index.html*" --cut-dirs=10
+cd $PRE_PATH
+
+TRG_PATH=$TFTP_PATH/boot/ubuntu-$REL/ubuntu-installer/$ARCH
+[ ! -d $TRG_PATH ] && mkdir $TRG_PATH -p
+rsync -avP $TMP/$TRG_PKG/ $TRG_PATH --delete-after
+. ./common/clean.sh
+
